89bf166185544c9befd6ac49055394ef2f5f8b59
swaroop1611 <147691274+swaroop1611@users.noreply.github.com>
2023-10-15 23:37:38 +0530

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,67 @@
+//prompt("enter your name","");
+//console.log("this is console");
+//console.log(6+5);
+//document.write("<h1> javascript</h1>");
+
+// var num1=5;
+// var num1=6;//we can redeclare the variable 
+// num1=8//we can reassign the variable 
+// console.log(num1);
+
+// let num2=9
+// let num2=10//we can redecalrethe variables
+// num2=11//we can reassign the variable 
+// console.log(num2);
+
+// const num3=10
+// const num3=11//we can redeclare the variable
+// num3=15
+// console.log(num3);
+
+// console.log(3**4)
+//let x=5
+// x++
+//console.log(x);
+//x--
+//console.log(x);
+
+// let num1=5
+// let num2="5"
+// console.log(num1==num2);
+// console.log(num1==num2);
+
+// function mul(n1,n2,n3,n4){
+//     return n1*n2*n3*n4;
+
+// }
+// document.write(mul(4,5,6,7))
+
+// function greet(){
+//     alert("hello everyone");
+// }
+
+// let age=20
+// if(age>=18)
+// {
+//     document.write("eligible for vote");
+
+// }else{
+//     document .write("not eligible ");
+// }
+
+// for(let num =10;num>=1;num--){
+//     console.log(num);
+// }
+
+// dom-document object model
+// dom manipulation methods
+// document .getElementsbyId()
+// document .getElementsByClassName()
+// document.querySelector()
+// document.querySelectorAll()
+
+// let el=document.getElementbyId("head").innerHTML
+// console.log(el);
+
+let ele= document.querySelector("#head").innerHTML;
+ console.log(ele);
