10ad8e69c4831baf17245359d04f99f4b3c63754
swaroop1611 <147691274+swaroop1611@users.noreply.github.com>
2023-10-20 10:03:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <Body/>
+    <Footer/>
+    </>
+  );
+};
+export default App;
